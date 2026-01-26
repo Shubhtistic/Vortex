5b02921e1f69718ad157910d02b7e563b7b696ae
Shubham <theshubham1412@gmail.com>
2026-01-26 22:04:23 +0530

    Update: modified config.py to bifurcate Postgresql Credentials

--- app/core/config.py
+++ app/core/config.py
@@ -8,13 +8,21 @@ class Settings(BaseSettings):
 
     # database url
     # our appp wiill crash without this
-    DATABASE_URL: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
     REDIS_URL: str
 
     class Config:
         # Tell Pydantic to read the file named .env
         env_file = ".env"
+
+    @property
+    def POSTGRES_URL(self):
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
 
 # Instantiate the settings once to be imported everywhere
