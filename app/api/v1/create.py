ad2764cbaae77f627e594c5653d453f618dae4f8
Shubham <theshubham1412@gmail.com>
2026-01-26 22:22:46 +0530

    Update: Modified create.py endpoint to follow new async db sessions

--- app/api/v1/create.py
+++ app/api/v1/create.py
@@ -1,15 +1,36 @@
-from fastapi import APIRouter  ##   ,HTTPException
+from fastapi import APIRouter, Depends
 from app.schemas.event import CreateEvent
+from app.models.event_table import Event
+from app.db.session import get_session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
 
 
 @router.post("/track")
-async def track_event(event: CreateEvent):
-    print(f"✅ RECEIVED EVENT: {event.event_type} on {event.url}")
+async def track_new_event(
+    event_in: CreateEvent, session: AsyncSession = Depends(get_session)
+):
+    """
+    Docstring for track_new_event
 
-    return {
-        "status": "received",
-        "event_id": event.request_id,
-        "processed_at": event.timestamp,
-    }
+    :param event_in: new event to be added in database
+    :type event_in: CreateEvent
+    :param session: db session to perform operations
+    :type session: AsyncSession
+    """
+    # Convert Schema -> Dictionary
+    data = event_in.model_dump()
+
+    # Postgres cannot store the Pydantic 'HttpUrl' object directly.
+    data["url"] = str(data["url"])
+
+    event_db = Event(**data)
+    # ** means unpack a dictionary into keywrd arguments
+    # **dict  →  spread the dict into arguments
+
+    session.add(event_db)
+    await session.commit()
+    await session.refresh(event_db)
+
+    return {"status": "saved", "event_id": event_db.id, "timestamp": event_db.timestamp}
