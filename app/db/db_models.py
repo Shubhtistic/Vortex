33620143ae72239a0abb4e21edcda1290c052b8b
Shubham <theshubham1412@gmail.com>
2026-02-21 23:46:23 +0530

    Refactor(db): renamed event_table to db_models and moved it to db folder as now we have multiple tables its good to hold all table defns in one file also makes it easy for us to do relationship based logic as all tables now are organised in one file

--- app/db/db_models.py
+++ app/db/db_models.py
@@ -1,10 +1,11 @@
+from enum import Enum
 from sqlmodel import SQLModel, Field, JSON
 from typing import Optional, Dict, Any
 from sqlalchemy import Column, DateTime
 from datetime import datetime, timezone
 import uuid
 
-# table = true -> make an tabke is absent
+# table = true -> make an table is absent
 
 
 class Event(SQLModel, table=True):
@@ -27,3 +28,27 @@ class Event(SQLModel, table=True):
     )
     # # sa_column tells sqlodel to use a specific SQLAlchemy column type
     # DateTime(timezone=True) creates a 'TIMESTAMPTZ' column in Postgres
+
+
+class KeyType(str, Enum):
+    publishable = "publishable"
+    secret = "secret"
+
+
+class ApiKey(SQLModel, table=True):
+
+    # unique id
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+
+    tenant_id: str = Field(index=True)
+    hashed_key: str = Field(index=True, unique=True)
+
+    key_type: KeyType
+
+    # turn off bad/malicious key
+    is_active: bool = Field(default=True)
+
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(DateTime(timezone=True)),
+    )
