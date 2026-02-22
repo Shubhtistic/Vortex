9409e6ef1fb68e44c5501f6c7ef8a7f251b2db3e
Shubham <theshubham1412@gmail.com>
2026-02-22 15:32:04 +0530

    Add(db): tenant_id field added in events table

--- app/db/db_models.py
+++ app/db/db_models.py
@@ -5,30 +5,6 @@ from sqlalchemy import Column, DateTime
 from datetime import datetime, timezone
 import uuid
 
-# table = true -> make an table is absent
-
-
-class Event(SQLModel, table=True):
-    __tablename__ = "events"
-
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-
-    # Index: We make 'url' searchable so queries are fast later
-    url: str = Field(index=True)
-
-    event_type: str
-
-    # sa_type=JSON is required to tell the database this is json object
-    payload: Optional[Dict[str, Any]] = Field(default={}, sa_type=JSON)
-
-    # 5. Timestamp: When did we save this?
-    timestamp: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc),
-        sa_column=Column(DateTime(timezone=True)),
-    )
-    # # sa_column tells sqlodel to use a specific SQLAlchemy column type
-    # DateTime(timezone=True) creates a 'TIMESTAMPTZ' column in Postgres
-
 
 class KeyType(str, Enum):
     publishable = "publishable"
@@ -36,19 +12,44 @@ class KeyType(str, Enum):
 
 
 class ApiKey(SQLModel, table=True):
+    __tablename__ = "api_keys"
 
     # unique id
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
 
+    # The string that logically links this key to the events
     tenant_id: str = Field(index=True)
-    hashed_key: str = Field(index=True, unique=True)
 
+    hashed_key: str = Field(index=True, unique=True)
     key_type: KeyType
 
     # turn off bad/malicious key
     is_active: bool = Field(default=True)
 
     created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(DateTime(timezone=True)),
+    )
+
+
+class Event(SQLModel, table=True):
+    __tablename__ = "events"
+
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+
+    # This is how we know whose event this is. It logically matches ApiKey.tenant_id
+    tenant_id: str = Field(index=True)
+
+    # indexed so queries are fast later
+    url: str = Field(index=True)
+
+    event_type: str
+
+    # sa_type=JSON is required to tell the database this is json object
+    payload: Optional[Dict[str, Any]] = Field(default={}, sa_type=JSON)
+
+    # Timestamp: When did we save this?
+    timestamp: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc),
         sa_column=Column(DateTime(timezone=True)),
     )
