84b714941818d88ede34b626528c506b7b430ef2
Shubham <theshubham1412@gmail.com>
2026-03-23 18:08:43 +0530

    Refactor: Added new mock database connection url

--- app/core/config.py
+++ app/core/config.py
@@ -7,12 +7,13 @@ class Settings(BaseSettings):
     VERSION: str = "0.1.0"
 
     # database url
-    # our appp wiill crash without this
+    # our app wiill crash without this
     POSTGRES_SERVER: str
     POSTGRES_PORT: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
+    POSTGRES_TEST_DB:str
 
     REDIS_URL: str
 
@@ -22,6 +23,9 @@ class Settings(BaseSettings):
 
     @property
     def POSTGRES_URL(self):
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+    @property
+    def TEST_POSTGRES_URL(self):
         return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
 
