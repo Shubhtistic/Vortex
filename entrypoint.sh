dde132fd00e2b4c27eb134d9af6969e1178303c6
Shubham <theshubham1412@gmail.com>
2026-04-18 23:23:28 +0530

    add migrate container, and fix docker startup
    - added separate migrate container to run alembic migrations once on startup
    - fixed docker startup order using depends_on conditions
    - cleaned up entrypoint.sh to only wait for postgres, removed migration logic from it
    - added env vars to healthcheck so nothing is hardcoded

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 set -e
 
-
-echo "Checking if Postgres is ready at postgres_database:5432..."
+echo "Checking if Postgres is ready at ${POSTGRES_SERVER}:5432..."
 
 # -h: host, -p: port, -U: user
-until pg_isready -h postgres_database -p 5432 -U "${POSTGRES_USER}"; do
+until pg_isready -h "${POSTGRES_SERVER}" -p 5432 -U "${POSTGRES_USER}"; do
   echo "Postgres is starting up... waiting"
   sleep 2s
 done
 
-
-# now role based log
-if [[ "$*" == *"uvicorn"* ]]; then
-    echo "api detected running migrations"
-    alembic upgrade head
-else
-    echo "worker detected"
-    sleep 2s
-fi
-
+echo "Postgres is ready!"
 
 # hand control to cmd
-# fastapi -> pid 1
+# fastapi/celery -> pid 1
 exec "$@"
