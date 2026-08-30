5dc0a04c02f7384199e66afffad24d7a44ab1a03
Shubhtistic <theshubham1412@gmail.com>
2026-08-30 18:03:54 +0530

    Add: add auth routes in api module

--- backend/src/vortex/api/routers.py
+++ backend/src/vortex/api/routers.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from src.vortex.organizations.routers import router as org_router
+from src.vortex.auth.routers import router as auth_router
 
 router = APIRouter()
 
 router.include_router(org_router)
+router.include_router(auth_router)
